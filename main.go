0639db9eb96a0fd6eaccbed2dd8548352cd572e2
Kakudo <31888089+kakudo415@users.noreply.github.com>
2018-08-02 01:28:15 +0900

    access log

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
+	"os/user"
+	"path/filepath"
 	"strings"
 
 	"./page"
@@ -9,6 +13,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		accessLog(r)
 		if r.URL.Path == "/" {
 			r.URL.Path = "/index"
 		}
@@ -24,4 +29,20 @@ func main() {
 	})
 	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("js"))))
 	http.ListenAndServe("127.0.0.1:8000", nil)
+}
+
+func accessLog(r *http.Request) {
+	log.Printf("%s => %s %s", r.RemoteAddr, r.Method, r.URL.Path)
+}
+
+func init() {
+	u, e := user.Current()
+	if e != nil {
+		panic(e)
+	}
+	f, e := os.OpenFile(filepath.Clean(u.HomeDir+"/.access"), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if e != nil {
+		panic(e)
+	}
+	log.SetOutput(f)
 }
