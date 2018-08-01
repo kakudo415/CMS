b7d68fd09bcc57dd6692ebe1b3489a777ce09040
Kakudo <31888089+kakudo415@users.noreply.github.com>
2018-08-02 02:07:27 +0900

    アクセスログもnginx任せ

--- main.go
+++ main.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"log"
 	"net/http"
-	"os"
-	"os/user"
-	"path/filepath"
 
 	"./page"
 	"github.com/tdewolff/minify"
@@ -14,7 +10,6 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		accessLog(r)
 		if r.URL.Path == "/" {
 			r.URL.Path = "/index"
 		}
@@ -29,20 +24,4 @@ func main() {
 	http.Handle("/js/", http.StripPrefix("/js/", min.Middleware(http.FileServer(http.Dir("js")))))
 
 	http.ListenAndServe("127.0.0.1:8000", nil)
-}
-
-func accessLog(r *http.Request) {
-	log.Printf("%s => %s %s\n", r.RemoteAddr, r.Method, r.URL.Path)
-}
-
-func init() {
-	u, e := user.Current()
-	if e != nil {
-		panic(e)
-	}
-	f, e := os.OpenFile(filepath.Clean(u.HomeDir+"/.access"), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if e != nil {
-		panic(e)
-	}
-	log.SetOutput(f)
 }
