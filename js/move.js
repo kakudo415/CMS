b6f34b116f5c7ffadd0bd3c982a94e8327d75763
Kakudo <31888089+kakudo415@users.noreply.github.com>
2018-08-04 14:34:03 +0900

    move.js refactoring

--- js/move.js
+++ js/move.js
@@ -1,90 +1,74 @@
 "use strict";
 
-window.onload = () => {
-	const move = () => {
-		let contentHTML = document.getElementById("content");
-
-		let Ajax = {
-			XHR: new XMLHttpRequest(),
-			Path: "",
-			Time: Date.now(),
-			Data: ""
-		};
-
-		const mouseEnter = (ev) => {
-			if (isLocal(ev.target.href)) {
-				ajaxGET(ev.target.href);
-			}
-		};
-
-		const mouseUp = (ev) => {
-			if (ev.button === 0) {
-				if (isLocal(ev.target.href)) {
-					if (Ajax.Path !== ev.target.href) {
-						ajaxGET(ev.target.href);
-					}
-					let count = 0;
-					let timer = setInterval(() => {
-						if (Ajax.Data !== null) {
-							movePage();
-							clearTimeout(timer);
-						}
-						count++;
-						if (count > 300) {
-							clearTimeout(timer);
-							location.href = Ajax.Path;
-						}
-					}, 10);
-				} else {
-					location.href = ev.target.href;
-				}
-			}
-		};
-
-		const popState = (ev) => {
-			location.reload();
-		};
-
-		const ajaxGET = async (url) => {
-			if (Ajax.Path !== url && (Ajax.Time + 100) < Date.now()) {
-				await Ajax.XHR.abort();
-				Ajax.XHR.responseType = "document";
-				Ajax.XHR.onloadstart = (ev) => {
-					Ajax.Path = url;
-					Ajax.Time = Date.now();
-					Ajax.Data = null;
-				};
-				Ajax.XHR.onload = (ev) => {
-					Ajax.Data = ev.target.responseXML;
-				};
-				Ajax.XHR.open("GET", url + "?r=i", true);
-				Ajax.XHR.send();
-			}
-		};
-
-		const movePage = () => {
-			document.title = Ajax.Data.title;
-			contentHTML.innerHTML = Ajax.Data.body.innerHTML;
-			addEvent();
-			history.pushState(null, null, Ajax.Path);
-		};
-
-		const isLocal = (URL) => {
-			return URL.startsWith("http://" + document.domain) || URL.startsWith("https://" + document.domain) || URL.startsWith("//" + document.domain) || URL.startsWith("/");
-		};
-
-		const addEvent = () => {
-			for (let tag of document.getElementsByTagName("a")) {
-				tag.onmouseenter = mouseEnter;
-				tag.onmouseup = mouseUp;
-				tag.onclick = () => { return false };
-			}
-		};
-		window.onpopstate = popState;
-		addEvent();
+if (history.pushState && history.state !== undefined) {
+	let contentHTML = document.getElementById("content");
+	let Ajax = {
+		XHR: new XMLHttpRequest(),
+		Path: "",
+		Time: Date.now(),
+		Data: null
 	};
 
-	if (history.pushState && history.state !== undefined) {
-		move();
-	}
+	const isLocal = (uri) => {
+		return uri.startsWith("http://" + document.domain) || uri.startsWith("https://" + document.domain) || uri.startsWith("//" + document.domain) || uri.startsWith("/");
+	};
+
+	const preGET = async (uri) => {
+		if (Ajax.Path !== uri && (Ajax.Time + 100) < Date.now()) {
+			await Ajax.XHR.abort();
+			Ajax.XHR.responseType = "document";
+			Ajax.XHR.onloadstart = (ev) => {
+				Ajax.Path = uri;
+				Ajax.Time = Date.now();
+			};
+			Ajax.XHR.onload = (ev) => {
+				Ajax.Data = ev.target.responseXML;
+			};
+			Ajax.XHR.open("GET", uri + "?r=i", true);
+			Ajax.XHR.send();
+		}
+	};
+
+	const mouseOver = (ev) => {
+		if (isLocal(ev.target.href)) {
+			preGET(ev.target.href);
+		}
+	};
+
+	const mouseDown = (ev) => {
+		if (ev.button === 0) {
+			if (isLocal(ev.target.href)) {
+				if (Ajax.Path !== ev.target.href) {
+					preGET(ev.target.href);
+				}
+				let count = 0;
+				let timer = setInterval(() => {
+					if (Ajax.Data !== null) {
+						clearTimeout(timer);
+						document.title = Ajax.Data.title;
+						contentHTML.innerHTML = Ajax.Data.body.innerHTML;
+						history.pushState(null, null, Ajax.Path);
+						aTagInit();
+					}
+					count++;
+					if (count > 300) {
+						clearTimeout(timer);
+						location.href = Ajax.Path;
+					}
+				}, 10);
+			} else {
+				location.href = ev.target.href;
+			}
+		}
+	};
+
+	const aTagInit = () => {
+		for (let tag of document.getElementsByTagName("a")) {
+			tag.onmouseover = mouseOver;
+			tag.onmousedown = mouseDown;
+			tag.onclick = () => { return false };
+		}
+	};
+
+	aTagInit();
 };
